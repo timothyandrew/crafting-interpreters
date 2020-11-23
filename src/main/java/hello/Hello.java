a53c36f5c0913835bca7a99113a14e151f3d86b1
Timothy Andrew <mail@timothyandrew.net>
2020-11-22 22:46:21 -0500

    autocomplete

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,7 +1,10 @@
 package hello;
 
+import java.util.ArrayList;
+
 public class Hello {
-  public static void main(String[] args) {
-  System.out.println("hi!");
-  }
+    public static void main(String[] args) {
+	ArrayList<Boolean> x = new ArrayList<>();
+	System.out.println(x.toString());
+    }
 }
